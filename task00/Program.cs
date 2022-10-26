d267bdba764b0ac42e5bd6927b4b273dff4074b0
Svetlana_Ivanova <nika.svetlanka@mail.ru>
2022-10-26 18:57:09 +0300

    Добавили задачи 01,02

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. на вход принимает число и 
+// 2. выдаёт его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = int.TryParse; // проверка на целочисленные значения
+
+// string str = Console.ReadLine(); - строка 
+// int n = Convert.ToInt32(str);
+
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} = {square}");
+
+// bool res = num == 5 && num < 10; // true - false
+// if (res)
+// {
+
+// }
---
+++ task02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1) Console.Write("Понедельник");
+if (a == 2) Console.Write("Вторник");
+if (a == 3) Console.Write("Среда");
+if (a == 4) Console.Write("Четверг");
+if (a == 5) Console.Write("Пятница");
+if (a == 6) Console.Write("Суббота");
+if (a == 7) Console.Write("Воскресенье");
+if (a > 7 || a < 1) Console.Write("Такого дня не существует");
