f13a2f113f8a5d94442c963f9834677abdd80f48
Svetlana_Ivanova <nika.svetlanka@mail.ru>
2022-11-10 11:27:13 +0300

    Отдельный репозиторий с задачами

--- task02/Program.cs
+++ task02/Program.cs
@@ -3,16 +3,26 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
 
-if (a > 0 && a < 8);
-{if (a == 1) Console.Write("Понедельник");
-    else if (a == 2) Console.Write("Вторник");
-    else if (a == 3) Console.Write("Среда");
-    else if (a == 4) Console.Write("Четверг");
-    else if (a == 5) Console.Write("Пятница");
-    else if (a == 6) Console.Write("Суббота");
-    else Console.Write("Воскресенье");
+        if (a <= 0 || a >= 8)
+        {
+        }
+        else
+        {
+            if (a == 1) Console.Write("Понедельник");
+            else if (a == 2) Console.Write("Вторник");
+            else if (a == 3) Console.Write("Среда");
+            else if (a == 4) Console.Write("Четверг");
+            else if (a == 5) Console.Write("Пятница");
+            else if (a == 6) Console.Write("Суббота");
+            else Console.Write("Воскресенье");
+        }
+        Console.Write("Такого дня не существует");
+    }
 }
-Console.Write("Такого дня не существует");
