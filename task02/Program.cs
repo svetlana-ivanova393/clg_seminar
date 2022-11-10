45a4c05eebe79d183d6dc3665be2cd0a70a98043
Svetlana_Ivanova <nika.svetlanka@mail.ru>
2022-11-10 11:00:04 +0300

    Задача 2

--- task02/Program.cs
+++ task02/Program.cs
@@ -6,11 +6,13 @@
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a == 1) Console.Write("Понедельник");
-if (a == 2) Console.Write("Вторник");
-if (a == 3) Console.Write("Среда");
-if (a == 4) Console.Write("Четверг");
-if (a == 5) Console.Write("Пятница");
-if (a == 6) Console.Write("Суббота");
-if (a == 7) Console.Write("Воскресенье");
-if (a > 7 || a < 1) Console.Write("Такого дня не существует");
+if (a > 0 && a < 8);
+{if (a == 1) Console.Write("Понедельник");
+    else if (a == 2) Console.Write("Вторник");
+    else if (a == 3) Console.Write("Среда");
+    else if (a == 4) Console.Write("Четверг");
+    else if (a == 5) Console.Write("Пятница");
+    else if (a == 6) Console.Write("Суббота");
+    else Console.Write("Воскресенье");
+}
+Console.Write("Такого дня не существует");
